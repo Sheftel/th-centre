b650181c39918879f85ca0796bdf6070741249f5
sheftel <sheftel.ks@gmail.com>
2023-12-13 23:23:44 +0700

    Update news mail task

--- app/news_and_places/tasks/news.py
+++ app/news_and_places/tasks/news.py
@@ -1,8 +1,10 @@
+from datetime import date
+
 from django.core.mail import send_mass_mail
 from constance import config
 
 from app import celery_app
-
+from ..models import News
 """
     Вопросы:
         1. Как выдавать информацию о новостях за день, если текст сообщения в constance статичен?
@@ -12,9 +14,10 @@ from app import celery_app
 
 @celery_app.task
 def send_news_mail():
+    news_for_today = News.objects.filter(created_at__date=date.today()).order_by('-created_at').values_list('title', flat=True).all()[:10]
     send_mass_mail((
         (config.NEWS_MAIL_SUBJECT,
-         config.NEWS_MAIL_TEXT,
+         config.NEWS_MAIL_TEXT+'\n'.join(news_for_today),
          'from@mail.com',
          config.NEWS_MAIL_RECIPIENTS.split(','),),
     ))
